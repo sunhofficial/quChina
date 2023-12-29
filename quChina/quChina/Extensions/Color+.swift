3de6335a69783735d88949779ca0af417ca246ed
Sunho <sunh803@naver.com>
2023-12-30 01:57:02 +0900

    feat: project setting

---
+++ quChina/quChina/Extensions/Color+.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  quChina
+//
+//  Created by 235 on 12/30/23.
+//
+
+import Foundation
---
+++ quChina/quChina/quChinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  quChinaApp.swift
+//  quChina
+//
+//  Created by 235 on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct quChinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
