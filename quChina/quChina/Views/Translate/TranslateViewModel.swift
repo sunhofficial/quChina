6e05816e4398e6b8af4f3493b0b106255508c467
Sunho <sunh803@naver.com>
2024-01-07 22:14:42 +0900

    feat: makeUI translateView

---
+++ quChina/quChina/Views/Translate/TranslateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TranslateViewModel.swift
+//  quChina
+//
+//  Created by 235 on 1/7/24.
+//
+
+import Foundation
---
+++ quChina/quChina/Views/Triangle.swift
@@ -0,0 +1,18 @@
+//
+//  TriangleView.swift
+//  quChina
+//
+//  Created by 235 on 1/2/24.
+//
+
+import SwiftUI
+
+struct TriangleView: Shape {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriangleView()
+}
